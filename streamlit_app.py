53be1c1312d183261af0fc5698ebd02223180330
sc3458016 <104919015+sc3458016@users.noreply.github.com>
2022-05-04 17:09:46 +0100

    Update streamlit_app.py
    Add header, text & pick list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,13 @@
 import pandas
 import streamlit
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
